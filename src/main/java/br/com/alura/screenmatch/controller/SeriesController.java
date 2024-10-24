c4d8ae4a608d9e52b0d292093676382b594bc608
mitugui <cristian.mitugui04@gmail.com>
2024-10-24 20:47:07 -0300

    refactor: use @RequestMapping in SeriesController to group series routes - Updated SeriesController to use @RequestMapping("/series"), simplifying route declarations.

--- src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
@@ -4,21 +4,23 @@ import br.com.alura.screenmatch.dto.SeriesDTO;
 import br.com.alura.screenmatch.services.SeriesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SeriesController {
     @Autowired
     private SeriesService service;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SeriesDTO> getSeries() {
         return service.getAllSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SeriesDTO> getTop5Series() {
         return service.getTop5Series();
     }
